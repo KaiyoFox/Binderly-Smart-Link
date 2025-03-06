3498af75da2a16b770c5d0297452cdf5a358d247
Peyton <83377575+PeytonIsHere@users.noreply.github.com>
2025-03-06 12:58:15 -0700

    Add files via upload

---
+++ kioskinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "adding something to a file....."
+
+sudo su
+
+# Append Chromium Kiosk Command to .service file
+echo "ExecStart=/usr/bin/chromium-browser --noerrdialogs --kiosk http://localhost" >> /home/%user/.config/systemd/binderly-kiosk.service
+
+# Hopefully make cursor invisible
+apt install unclutter
+echo "ExecStartPre=/usr/bin/unclutter -idle 0" >> /home/%user/.config/systemd/binderly-kiosk.servic
+
+# ----random notes----
+
+# Uncomment the following line to install Chromium browser if needed
+# apt install chromium-browser
+
+# This is the command to run chromium in kiosk mode. so make sure this works before leaving.
+# chromium-browser --noerrdialogs --kiosk http://localhost
+
+# home/%user/.config/systemd/binderly-kiosk.service
